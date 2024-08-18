ff1f38dd65c19ee8cbeefb8a6f24b37e292c3f77
Benevolence Messiah <120072200+BenevolenceMessiah@users.noreply.github.com>
2024-08-17 23:30:13 -0400

    add installer

---
+++ Install-unsloth.bat
@@ -0,0 +1,83 @@
+@echo off
+setlocal
+
+:::  ________  _______   ________   _______   ___      ___ ________  ___       _______   ________   ________  _______      
+::: |\   __  \|\  ___ \ |\   ___  \|\  ___ \ |\  \    /  /|\   __  \|\  \     |\  ___ \ |\   ___  \|\   ____\|\  ___ \     
+::: \ \  \|\ /\ \   __/|\ \  \\ \  \ \   __/|\ \  \  /  / | \  \|\  \ \  \    \ \   __/|\ \  \\ \  \ \  \___|\ \   __/|    
+:::  \ \   __  \ \  \_|/_\ \  \\ \  \ \  \_|/_\ \  \/  / / \ \  \\\  \ \  \    \ \  \_|/_\ \  \\ \  \ \  \    \ \  \_|/__  
+:::   \ \  \|\  \ \  \_|\ \ \  \\ \  \ \  \_|\ \ \    / /   \ \  \\\  \ \  \____\ \  \_|\ \ \  \\ \  \ \  \____\ \  \_|\ \ 
+:::    \ \_______\ \_______\ \__\\ \__\ \_______\ \__/ /     \ \_______\ \_______\ \_______\ \__\\ \__\ \_______\ \_______\
+:::     \|_______|\|_______|\|__| \|__|\|_______|\|__|/       \|_______|\|_______|\|_______|\|__| \|__|\|_______|\|_______|
+::: 
+:::  _____ ______   _______   ________   ________  ___  ________  ___  ___     
+::: |\   _ \  _   \|\  ___ \ |\   ____\ |\   ____\|\  \|\   __  \|\  \|\  \    
+::: \ \  \\\__\ \  \ \   __/|\ \  \___|_\ \  \___|\ \  \ \  \|\  \ \  \\\  \   
+:::  \ \  \\|__| \  \ \  \_|/_\ \_____  \\ \_____  \ \  \ \   __  \ \   __  \  
+:::   \ \  \    \ \  \ \  \_|\ \|____|\  \\|____|\  \ \  \ \  \ \  \ \  \ \  \ 
+:::    \ \__\    \ \__\ \_______\____\_\  \ ____\_\  \ \__\ \__\ \__\ \__\ \__\
+:::     \|__|     \|__|\|_______|\_________\\_________\|__|\|__|\|__|\|__|\|__|
+:::                             \|_________\|_________|
+::: ___  ________   ________  _________  ________  ___       ___       _______   ________     
+::: |\  \|\   ___  \|\   ____\|\___   ___\\   __  \|\  \     |\  \     |\  ___ \ |\   __  \    
+::: \ \  \ \  \\ \  \ \  \___|\|___ \  \_\ \  \|\  \ \  \    \ \  \    \ \   __/|\ \  \|\  \   
+:::  \ \  \ \  \\ \  \ \_____  \   \ \  \ \ \   __  \ \  \    \ \  \    \ \  \_|/_\ \   _  _\  
+:::   \ \  \ \  \\ \  \|____|\  \   \ \  \ \ \  \ \  \ \  \____\ \  \____\ \  \_|\ \ \  \\  \| 
+:::    \ \__\ \__\\ \__\____\_\  \   \ \__\ \ \__\ \__\ \_______\ \_______\ \_______\ \__\\ _\ 
+:::     \|__|\|__| \|__|\_________\   \|__|  \|__|\|__|\|_______|\|_______|\|_______|\|__|\|__|
+:::                    \|_________|
+
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+
+timeout /t 3
+
+:: Download the code
+echo As-salamu alaykum!!
+echo detecting presence of repo, git cloning if not detected...
+echo ---------------------------------------------------------------
+if exist unsloth\ goto Install
+git clone https://github.com/BenevolenceMessiah/unsloth.git
+cd unsloth
+git pull
+echo ---------------------------------------------------------------
+
+:: Create and activate a Python virtual environment
+:Install
+echo Creating virtual environment
+echo ---------------------------------------------------------------
+if not exist venv (
+    py -3.10 -m venv .venv
+) else (
+    echo Existing venv detected. Activating.
+)
+echo Activating virtual environment
+call .venv\Scripts\activate
+echo ---------------------------------------------------------------
+
+:: Install dependencies
+echo Installing dependencies...
+ pip install --upgrade --force-reinstall --no-cache-dir torch==2.3.0 --index-url https://download.pytorch.org/whl/cu121
+ pip install bitsandbytes
+ pip install xformers
+ pip install numpy --upgrade --force-reinstall --no-cache-dir
+ pip install unsloth[colab-new]
+:: pip install unsloth
+
+:: Launch
+echo Launching...
+echo ---------------------------------------------------------------
+if not exist venv (
+    py -3.10 -m venv .venv
+) else (
+    echo Existing venv detected. Activating.
+)
+echo Activating virtual environment
+call .venv\Scripts\activate
+echo ---------------------------------------------------------------
+call python unsloth-cli.py
+
+
+:End 
+echo ---------------------------------------------------------------
+echo As-salamu alaykum!!
+echo ---------------------------------------------------------------
+pause
